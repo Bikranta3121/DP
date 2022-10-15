25090f6c7545cc52698ff4080417c836dc77323c
BIKRANTA KUMAR BEHERA <106620478+Bikranta3121@users.noreply.github.com>
2022-10-15 18:38:33 +0530

    Create climbing stairs.cpp

---
+++ EASY LEETCODE/climbing stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     //memoization
+    int f(int i,vector<int> &dp)
+    {
+        if(i==0) return 1;
+        if(i<0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take=f(i-1,dp);
+        int notake=0;
+        if(i>0) notake=f(i-2,dp);
+        return dp[i]=take+notake;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
+    }
+};
